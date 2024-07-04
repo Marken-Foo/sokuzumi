6ee734f87e1ab7e9af1617682998f5093d9263a1
Marken Foo <41288521+Marken-Foo@users.noreply.github.com>
2024-07-04 12:41:22 +0800

    Display koma on board based on KomaType and direction

--- app/src/main/java/com/mfoo/sokuzumi/MainActivity.kt
+++ app/src/main/java/com/mfoo/sokuzumi/MainActivity.kt
@@ -73,12 +73,16 @@ fun Board(modifier: Modifier = Modifier) {
             Modifier.size(boardWidth, boardHeight)
         )
         Koma(
+            KomaType.KE,
+            true,
             modifier = Modifier
                 .width(sqWidth)
                 .height(sqHeight)
                 .offset(sqX(7), sqY(7))
         )
         Koma(
+            KomaType.NG,
+            false,
             modifier = Modifier
                 .width(sqWidth)
                 .height(sqHeight)
@@ -87,9 +91,51 @@ fun Board(modifier: Modifier = Modifier) {
     }
 }
 
+fun getKomaImage(komaType: KomaType, isUpsideDown: Boolean): Int {
+    return when (isUpsideDown) {
+        false -> when (komaType) {
+            KomaType.FU -> R.drawable.koma__kanji_light__0fu
+            KomaType.KY -> R.drawable.koma__kanji_light__0ky
+            KomaType.KE -> R.drawable.koma__kanji_light__0ke
+            KomaType.GI -> R.drawable.koma__kanji_light__0gi
+            KomaType.KI -> R.drawable.koma__kanji_light__0ki
+            KomaType.KA -> R.drawable.koma__kanji_light__0ka
+            KomaType.HI -> R.drawable.koma__kanji_light__0hi
+            KomaType.OU -> R.drawable.koma__kanji_light__0ou
+            KomaType.TO -> R.drawable.koma__kanji_light__0to
+            KomaType.NY -> R.drawable.koma__kanji_light__0ny
+            KomaType.NK -> R.drawable.koma__kanji_light__0nk
+            KomaType.NG -> R.drawable.koma__kanji_light__0ng
+            KomaType.UM -> R.drawable.koma__kanji_light__0um
+            KomaType.RY -> R.drawable.koma__kanji_light__0ry
+        }
+
+        true -> when (komaType) {
+            KomaType.FU -> R.drawable.koma__kanji_light__1fu
+            KomaType.KY -> R.drawable.koma__kanji_light__1ky
+            KomaType.KE -> R.drawable.koma__kanji_light__1ke
+            KomaType.GI -> R.drawable.koma__kanji_light__1gi
+            KomaType.KI -> R.drawable.koma__kanji_light__1ki
+            KomaType.KA -> R.drawable.koma__kanji_light__1ka
+            KomaType.HI -> R.drawable.koma__kanji_light__1hi
+            KomaType.OU -> R.drawable.koma__kanji_light__1ou
+            KomaType.TO -> R.drawable.koma__kanji_light__1to
+            KomaType.NY -> R.drawable.koma__kanji_light__1ny
+            KomaType.NK -> R.drawable.koma__kanji_light__1nk
+            KomaType.NG -> R.drawable.koma__kanji_light__1ng
+            KomaType.UM -> R.drawable.koma__kanji_light__1um
+            KomaType.RY -> R.drawable.koma__kanji_light__1ry
+        }
+    }
+}
+
 @Composable
-fun Koma(modifier: Modifier = Modifier) {
-    val painter = painterResource(id = R.drawable.koma__kanji_light__0fu)
+fun Koma(
+    komaType: KomaType,
+    isUpsideDown: Boolean,
+    modifier: Modifier = Modifier,
+) {
+    val painter = painterResource(id = getKomaImage(komaType, isUpsideDown))
     Image(painter = painter, contentDescription = null, modifier = modifier)
 }
 
