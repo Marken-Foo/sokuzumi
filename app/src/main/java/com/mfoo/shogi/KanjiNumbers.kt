ba4bee92ffabe5b8663440701385fb3f62958c21
Marken Foo <41288521+Marken-Foo@users.noreply.github.com>
2024-07-11 10:48:56 +0800

    Read numbers between 1-99 from kanji

---
+++ app/src/main/java/com/mfoo/shogi/KanjiNumbers.kt
@@ -0,0 +1,37 @@
+package com.mfoo.shogi
+
+object KanjiNumbers {
+    private fun kanjiToInt(string: String): Int? {
+        return when (string) {
+            "一" -> 1
+            "二" -> 2
+            "三" -> 3
+            "四" -> 4
+            "五" -> 5
+            "六" -> 6
+            "七" -> 7
+            "八" -> 8
+            "九" -> 9
+            "十" -> 10
+            else -> null
+        }
+    }
+
+    private val kanjiNumRegex =
+        Regex("""(?<TensGroup>(?<Tens>[二三四五六七八九])?十)?(?<Units>[一二三四五六七八九])?""")
+
+    /**
+     * Returns the value of an integer in kanji between 1 and 99 inclusive.
+     */
+    fun read(string: String): Int? {
+        val match = kanjiNumRegex.matchEntire(string)
+        val unitsValue = match?.groups?.get("Units")?.value?.let(::kanjiToInt)
+        val hasTens = match?.groups?.get("TensGroup") != null
+        val tens = match?.groups?.get("Tens")?.value?.let(::kanjiToInt) ?: 1
+        return if (hasTens) {
+            10 * tens + (unitsValue ?: 0)
+        } else {
+            unitsValue
+        }
+    }
+}
---
+++ app/src/test/java/com/mfoo/shogi/KanjiNumbersTests.kt
@@ -0,0 +1,42 @@
+package com.mfoo.shogi
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+val kanjiMap = mapOf(
+    1 to "一",
+    2 to "二",
+    3 to "三",
+    4 to "四",
+    5 to "五",
+    6 to "六",
+    7 to "七",
+    8 to "八",
+    9 to "九",
+    10 to "十"
+)
+
+class KanjiNumbersTests : FunSpec({
+    context("Numbers from 1 to 10") {
+        for ((expected, kanji) in kanjiMap) {
+            test("Read ${kanji} as ${expected}") {
+                val result = KanjiNumbers.read(kanji)
+                result shouldBe expected
+            }
+        }
+    }
+
+    context("Numbers from 11 to 99") {
+        for (expected in listOf(12, 18, 20, 37, 40, 55, 61, 79, 84, 93)) {
+            val tensKanji = (expected / 10)
+                .let { if (it == 1) "" else kanjiMap[it] }
+            val onesKanji = (expected % 10)
+                .let { if (it == 0) "" else kanjiMap[it] }
+            val kanji = tensKanji + kanjiMap[10] + onesKanji
+            test("Read ${kanji} as ${expected}") {
+                val result = KanjiNumbers.read(kanji)
+                result shouldBe expected
+            }
+        }
+    }
+})
