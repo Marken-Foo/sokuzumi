6002b2ad619fd748d97c9dbc554602df8c41a064
Marken Foo <41288521+Marken-Foo@users.noreply.github.com>
2024-07-21 16:10:30 +0800

    Add move validity tests for HI, add tests for drop and promotion validity

--- app/src/test/java/com/mfoo/shogi/ValidMoveTests.kt
+++ app/src/test/java/com/mfoo/shogi/ValidMoveTests.kt
@@ -81,6 +81,39 @@ private fun testRegularMoves(
     results.all { isValid -> isValid } shouldBe true
 }
 
+private fun testPromotionMoves(
+    komas: Map<Square, Koma>,
+    startSq: Square,
+    endSqs: Iterable<Square>,
+) {
+    val pos = Pos.fromMap(komas)
+    val koma = komas[startSq]
+    koma shouldNotBe null
+
+    val side = koma!!.side
+
+    val moves = endSqs.map {
+        Move.Regular(
+            startSq = startSq,
+            endSq = it,
+            isPromotion = true,
+            side = koma.side,
+            komaType = koma.komaType,
+            capturedKoma = komas[it]
+        )
+    }
+    val results = moves.map { move ->
+        isValid(move, pos.let {
+            if (side.isSente()) {
+                it
+            } else {
+                it.toggleSideToMove()
+            }
+        })
+    }
+    results.all { isValid -> isValid } shouldBe true
+}
+
 class ValidMoveTests : FunSpec({
     context("General tests") {
         test("Should not be able to drop koma if koma not in hand") {
@@ -88,6 +121,32 @@ class ValidMoveTests : FunSpec({
             val side = Side.SENTE
             val pos = Pos.empty().incrementHandAmount(side.switch(), komaType)
             val move = Move.Drop(sq(5, 5), side, komaType)
+            val result = isValid(move, pos)
+            val expected = false
+            result shouldBe expected
+        }
+
+        test("Should not be able to drop koma if ally on square") {
+            val komaType = KomaType.GI
+            val side = Side.SENTE
+            val sq = sq(5, 5)
+            val pos = Pos.empty()
+                .setKoma(sq, Koma(side, KomaType.NG))
+                .incrementHandAmount(side, komaType)
+            val move = Move.Drop(sq, side, komaType)
+            val result = isValid(move, pos)
+            val expected = false
+            result shouldBe expected
+        }
+
+        test("Should not be able to drop koma if enemy on square") {
+            val komaType = KomaType.GI
+            val side = Side.SENTE
+            val sq = sq(5, 5)
+            val pos = Pos.empty()
+                .setKoma(sq, Koma(side.switch(), KomaType.NG))
+                .incrementHandAmount(side, komaType)
+            val move = Move.Drop(sq, side, komaType)
             val result = isValid(move, pos)
             val expected = false
             result shouldBe expected
@@ -326,8 +385,8 @@ class ValidMoveTests : FunSpec({
             )
             val komas = mapOf(
                 startSq to Koma(side, KomaType.KI),
-                sq(8,8) to Koma(side, KomaType.KI),
-                sq(6,7) to Koma(side, KomaType.GI),
+                sq(8, 8) to Koma(side, KomaType.KI),
+                sq(6, 7) to Koma(side, KomaType.GI),
             )
 
             testRegularMoves(
@@ -350,8 +409,8 @@ class ValidMoveTests : FunSpec({
             )
             val komas = mapOf(
                 startSq to Koma(side, KomaType.KI),
-                sq(8,8) to Koma(side.switch(), KomaType.KI),
-                sq(6,7) to Koma(side.switch(), KomaType.GI),
+                sq(8, 8) to Koma(side.switch(), KomaType.KI),
+                sq(6, 7) to Koma(side.switch(), KomaType.GI),
             )
 
             testRegularMoves(
@@ -375,6 +434,113 @@ class ValidMoveTests : FunSpec({
             )
             val result = isValid(move, pos)
             result shouldBe expected
+        }
+    }
+
+    context("HI moves") {
+        test("Regular step") {
+            val side = Side.GOTE
+            val startSq = sq(3, 4)
+            val endSqs = (1..9)
+                .fold(emptySet<Square>()) { acc, n ->
+                    acc
+                        .plus(Square(startSq.col, Row(n)))
+                        .plus(Square(Col(n), startSq.row))
+                }
+                .minus(startSq)
+            val komas = mapOf(
+                startSq to Koma(side, KomaType.HI),
+            )
+
+            testRegularMoves(
+                komas,
+                startSq = startSq,
+                endSqs = endSqs,
+            )
+        }
+        test("Blocked") {
+            val side = Side.GOTE
+            val startSq = sq(3, 4)
+            val komas = mapOf(
+                startSq to Koma(side, KomaType.HI),
+                sq(3, 1) to Koma(side, KomaType.KE),
+                sq(3, 7) to Koma(side, KomaType.TO),
+                sq(2, 4) to Koma(side, KomaType.UM),
+                sq(7, 4) to Koma(side, KomaType.NY),
+            )
+            val endSqs = setOf(
+                sq(3, 2), sq(3, 3),
+                sq(3, 5), sq(3, 6),
+                sq(4, 4), sq(5, 4), sq(6, 4),
+            )
+            testRegularMoves(komas, startSq = startSq, endSqs = endSqs)
+        }
+        // hi captures
+        test("Captures") {
+            val side = Side.GOTE
+            val startSq = sq(3, 4)
+            val komas = mapOf(
+                startSq to Koma(side, KomaType.HI),
+                sq(3, 1) to Koma(side.switch(), KomaType.KE),
+                sq(3, 7) to Koma(side.switch(), KomaType.TO),
+                sq(2, 4) to Koma(side.switch(), KomaType.UM),
+                sq(7, 4) to Koma(side.switch(), KomaType.NY),
+            )
+            val endSqs = setOf(
+                sq(3, 1), sq(3, 2), sq(3, 3),
+                sq(3, 5), sq(3, 6), sq(3, 7),
+                sq(2, 4),
+                sq(4, 4), sq(5, 4), sq(6, 4), sq(7, 4)
+            )
+            testRegularMoves(komas, startSq = startSq, endSqs = endSqs)
+        }
+
+        test("Sente promotions into promotion zone") {
+            val side = Side.SENTE
+            val startSq = sq(3, 4)
+            val komas = mapOf(startSq to Koma(side, KomaType.HI))
+            val endSqs = setOf(
+                sq(3, 1), sq(3, 2), sq(3, 3)
+            )
+            testPromotionMoves(komas, startSq = startSq, endSqs = endSqs)
+        }
+
+        test("Gote promotions into promotion zone") {
+            val side = Side.GOTE
+            val startSq = sq(3, 4)
+            val komas = mapOf(startSq to Koma(side, KomaType.HI))
+            val endSqs = setOf(
+                sq(3, 7), sq(3, 8), sq(3, 9)
+            )
+            testPromotionMoves(komas, startSq = startSq, endSqs = endSqs)
+        }
+
+        test("Sente promotions from promotion zone") {
+            val side = Side.SENTE
+            val startSq = sq(5, 1)
+            val komas = mapOf(startSq to Koma(side, KomaType.HI))
+            val endSqs = (1..9)
+                .fold(emptySet<Square>()) { acc, n ->
+                    acc
+                        .plus(Square(startSq.col, Row(n)))
+                        .plus(Square(Col(n), startSq.row))
+                }
+                .minus(startSq)
+            testPromotionMoves(komas, startSq = startSq, endSqs = endSqs)
+        }
+
+        test("Gote promotions from promotion zone") {
+            val side = Side.GOTE
+            val startSq = sq(2, 8)
+            val komas = mapOf(startSq to Koma(side, KomaType.HI))
+            val endSqs = (1..9)
+                .fold(emptySet<Square>()) { acc, n ->
+                    acc
+                        .plus(Square(startSq.col, Row(n)))
+                        .plus(Square(Col(n), startSq.row))
+                }
+                .minus(startSq)
+            testPromotionMoves(komas, startSq = startSq, endSqs = endSqs)
         }
     }
 })
