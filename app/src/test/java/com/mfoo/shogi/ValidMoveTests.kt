d58aa8862aed0a256e359865b17b416503dd6cbe
Marken Foo <41288521+Marken-Foo@users.noreply.github.com>
2024-07-21 15:11:41 +0800

    Add tests for KI move validity

--- app/src/test/java/com/mfoo/shogi/ValidMoveTests.kt
+++ app/src/test/java/com/mfoo/shogi/ValidMoveTests.kt
@@ -48,6 +48,39 @@ private fun testRegularMove(
     result shouldBe expected
 }
 
+private fun testRegularMoves(
+    komas: Map<Square, Koma>,
+    startSq: Square,
+    endSqs: Iterable<Square>,
+) {
+    val pos = Pos.fromMap(komas)
+    val koma = komas[startSq]
+    koma shouldNotBe null
+
+    val side = koma!!.side
+
+    val moves = endSqs.map {
+        Move.Regular(
+            startSq = startSq,
+            endSq = it,
+            isPromotion = false,
+            side = koma.side,
+            komaType = koma.komaType,
+            capturedKoma = komas[it]
+        )
+    }
+    val results = moves.map { move ->
+        isValid(move, pos.let {
+            if (side.isSente()) {
+                it
+            } else {
+                it.toggleSideToMove()
+            }
+        })
+    }
+    results.all { isValid -> isValid } shouldBe true
+}
+
 class ValidMoveTests : FunSpec({
     context("General tests") {
         test("Should not be able to drop koma if koma not in hand") {
@@ -227,6 +260,114 @@ class ValidMoveTests : FunSpec({
                 .setKoma(sq(5, 6), Koma(side, KomaType.TO))
                 .setKoma(sq(4, 3), Koma(side, komaType))
                 .incrementHandAmount(side, komaType)
+            val move = Move.Drop(
+                sq = sq,
+                side = side,
+                komaType = komaType,
+            )
+            val result = isValid(move, pos)
+            result shouldBe expected
+        }
+    }
+
+    context("KI moves") {
+        test("Sente KI step") {
+            val side = Side.SENTE
+            val startSq = sq(7, 8)
+            val endSqs = listOf(
+                sq(8, 7),
+                sq(7, 7),
+                sq(6, 7),
+                sq(8, 8),
+                sq(6, 8),
+                sq(7, 9)
+            )
+            val komas = mapOf(
+                startSq to Koma(side, KomaType.KI),
+            )
+
+            testRegularMoves(
+                komas,
+                startSq = startSq,
+                endSqs = endSqs,
+            )
+        }
+
+        test("Gote KI step") {
+            val side = Side.GOTE
+            val startSq = sq(3, 5)
+            val endSqs = listOf(
+                sq(3, 4),
+                sq(4, 5),
+                sq(2, 5),
+                sq(4, 6),
+                sq(3, 6),
+                sq(2, 6)
+            )
+            val komas = mapOf(
+                startSq to Koma(side, KomaType.KI),
+            )
+
+            testRegularMoves(
+                komas,
+                startSq = startSq,
+                endSqs = endSqs,
+            )
+        }
+
+        test("Blocked sente KI step") {
+            val side = Side.SENTE
+            val startSq = sq(7, 8)
+            val endSqs = listOf(
+                sq(8, 7),
+                sq(7, 7),
+                sq(6, 8),
+                sq(7, 9)
+            )
+            val komas = mapOf(
+                startSq to Koma(side, KomaType.KI),
+                sq(8,8) to Koma(side, KomaType.KI),
+                sq(6,7) to Koma(side, KomaType.GI),
+            )
+
+            testRegularMoves(
+                komas,
+                startSq = startSq,
+                endSqs = endSqs,
+            )
+        }
+
+        test("Sente KI captures") {
+            val side = Side.SENTE
+            val startSq = sq(7, 8)
+            val endSqs = listOf(
+                sq(8, 7),
+                sq(7, 7),
+                sq(6, 7),
+                sq(8, 8),
+                sq(6, 8),
+                sq(7, 9)
+            )
+            val komas = mapOf(
+                startSq to Koma(side, KomaType.KI),
+                sq(8,8) to Koma(side.switch(), KomaType.KI),
+                sq(6,7) to Koma(side.switch(), KomaType.GI),
+            )
+
+            testRegularMoves(
+                komas,
+                startSq = startSq,
+                endSqs = endSqs,
+            )
+        }
+
+        test("Sente legal KI drop") {
+            val side = Side.SENTE
+            val komaType = KomaType.KI
+            val sq = sq(5, 3)
+            val expected = true
+
+            val pos = Pos.empty().incrementHandAmount(side, komaType)
             val move = Move.Drop(
                 sq = sq,
                 side = side,
