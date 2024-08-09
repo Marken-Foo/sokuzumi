3ee4499c4fa484a341bdd1bcba489286e31f2d2b
Marken Foo <41288521+Marken-Foo@users.noreply.github.com>
2024-08-09 11:15:43 +0800

    Move KIF to Game helper functions into GameImpl companion

--- app/src/main/java/com/mfoo/shogi/GameImpl.kt
+++ app/src/main/java/com/mfoo/shogi/GameImpl.kt
@@ -2,6 +2,7 @@ package com.mfoo.shogi
 
 import arrow.core.Either
 import com.mfoo.shogi.kif.KifAst
+import com.mfoo.shogi.kif.readKifFile
 
 
 private typealias MoveNum = Int
@@ -45,120 +46,6 @@ private data class Variation<T>(
     }
 }
 
-private fun moveFromKifAst(
-    kifAstMove: KifAst.Move,
-    startingMoveNum: Int,
-    startingSide: Side,
-    posBeforeMove: Position,
-): Move {
-    val side = getSideToMove(startingMoveNum, kifAstMove.moveNum, startingSide)
-    return when (kifAstMove) {
-        is KifAst.Move.Drop -> Move.Drop(
-            kifAstMove.sq,
-            side,
-            kifAstMove.komaType
-        )
-
-        is KifAst.Move.GameEnd -> {
-            val gameEndType = when (kifAstMove.endType) {
-                KifAst.Move.GameEndType.ABORT -> Move.GameEndType.ABORT
-                KifAst.Move.GameEndType.RESIGN -> Move.GameEndType.RESIGN
-                KifAst.Move.GameEndType.JISHOGI -> Move.GameEndType.JISHOGI
-                KifAst.Move.GameEndType.SENNICHITE -> Move.GameEndType.SENNICHITE
-                KifAst.Move.GameEndType.FLAG -> Move.GameEndType.FLAG
-                KifAst.Move.GameEndType.ILLEGAL_WIN -> Move.GameEndType.ILLEGAL_WIN
-                KifAst.Move.GameEndType.ILLEGAL_LOSS -> Move.GameEndType.ILLEGAL_LOSS
-                KifAst.Move.GameEndType.NYUUGYOKU -> Move.GameEndType.NYUUGYOKU
-                KifAst.Move.GameEndType.NO_CONTEST_WIN -> Move.GameEndType.NO_CONTEST_WIN
-                KifAst.Move.GameEndType.NO_CONTEST_LOSS -> Move.GameEndType.NO_CONTEST_LOSS
-                KifAst.Move.GameEndType.MATE -> Move.GameEndType.MATE
-                KifAst.Move.GameEndType.NO_MATE -> Move.GameEndType.NO_MATE
-            }
-            Move.GameEnd(gameEndType)
-        }
-
-        is KifAst.Move.Regular -> {
-            val capturedKoma =
-                posBeforeMove.getKoma(kifAstMove.endSq).getOrNull()
-            Move.Regular(
-                startSq = kifAstMove.startSq,
-                endSq = kifAstMove.endSq,
-                isPromotion = kifAstMove.isPromotion,
-                side = side,
-                komaType = kifAstMove.komaType,
-                capturedKoma = capturedKoma
-            )
-        }
-    }
-}
-
-private fun getSideToMove(
-    startingMoveNum: Int,
-    currentMoveNum: Int,
-    startingSide: Side,
-): Side {
-    // Assumes that moves alternate between sente and gote with no deviation
-    // (no passing moves, no game end, etc.)
-    return if ((startingMoveNum - currentMoveNum) % 2 == 0) {
-        startingSide
-    } else {
-        startingSide.switch()
-    }
-}
-
-private fun playOut(
-    variation: Variation<KifAst.Move>,
-    startingSide: Side,
-    startPos: Position,
-): Variation<Move> {
-    val movePositionPairs = variation.moves
-        .fold(emptyList<Pair<Move, Position>>()) { acc, kifAstMove ->
-            val pos = acc.lastOrNull()?.second ?: startPos
-            val move =
-                moveFromKifAst(kifAstMove, variation.moveNum, startingSide, pos)
-            acc + (move to pos.doMove(move))
-        }
-    val childVariations = variation.branches.mapValues { (idx, children) ->
-        children.map {
-            val sideToMove = getSideToMove(
-                variation.moveNum,
-                variation.moveNum + idx,
-                startingSide
-            )
-            playOut(it, sideToMove, movePositionPairs[idx].second)
-        }
-    }
-    return Variation(
-        variation.moveNum,
-        movePositionPairs.map { it.first },
-        childVariations
-    )
-}
-
-private fun traverse(
-    acc: Variation<KifAst.Move>,
-    node: KifAst.Tree.MoveNode,
-): Variation<KifAst.Move> {
-    if (node.children.isEmpty()) {
-        return acc.add(node.move)
-    }
-    val mainVariation = traverse(
-        acc.add(node.move), node.children[0]
-    )
-    return node.children.subList(1, node.children.size)
-        .fold(mainVariation) { a, n ->
-            a.addBranch(
-                traverse(
-                    Variation(
-                        1 + node.move.moveNum,
-                        emptyList(),
-                        emptyMap()
-                    ), n
-                )
-            )
-        }
-}
-
 private data class Choice(val moveNumber: MoveNum, val choice: Int)
 
 private data class CurrentLocation(
@@ -166,7 +53,7 @@ private data class CurrentLocation(
     val moveNumber: MoveNum,
 )
 
-class GameImpl private constructor(
+data class GameImpl private constructor(
     private val variations: Variation<Move>,
     private val path: CurrentLocation,
     private val pos: PositionImpl,
@@ -231,5 +118,132 @@ class GameImpl private constructor(
                 kifAst.startPos as PositionImpl,
             )
         }
+
+        private fun moveFromKifAst(
+            kifAstMove: KifAst.Move,
+            startingMoveNum: Int,
+            startingSide: Side,
+            posBeforeMove: Position,
+        ): Move {
+            val side =
+                getSideToMove(startingMoveNum, kifAstMove.moveNum, startingSide)
+            return when (kifAstMove) {
+                is KifAst.Move.Drop -> Move.Drop(
+                    kifAstMove.sq,
+                    side,
+                    kifAstMove.komaType
+                )
+
+                is KifAst.Move.GameEnd -> {
+                    val gameEndType = when (kifAstMove.endType) {
+                        KifAst.Move.GameEndType.ABORT -> Move.GameEndType.ABORT
+                        KifAst.Move.GameEndType.RESIGN -> Move.GameEndType.RESIGN
+                        KifAst.Move.GameEndType.JISHOGI -> Move.GameEndType.JISHOGI
+                        KifAst.Move.GameEndType.SENNICHITE -> Move.GameEndType.SENNICHITE
+                        KifAst.Move.GameEndType.FLAG -> Move.GameEndType.FLAG
+                        KifAst.Move.GameEndType.ILLEGAL_WIN -> Move.GameEndType.ILLEGAL_WIN
+                        KifAst.Move.GameEndType.ILLEGAL_LOSS -> Move.GameEndType.ILLEGAL_LOSS
+                        KifAst.Move.GameEndType.NYUUGYOKU -> Move.GameEndType.NYUUGYOKU
+                        KifAst.Move.GameEndType.NO_CONTEST_WIN -> Move.GameEndType.NO_CONTEST_WIN
+                        KifAst.Move.GameEndType.NO_CONTEST_LOSS -> Move.GameEndType.NO_CONTEST_LOSS
+                        KifAst.Move.GameEndType.MATE -> Move.GameEndType.MATE
+                        KifAst.Move.GameEndType.NO_MATE -> Move.GameEndType.NO_MATE
+                    }
+                    Move.GameEnd(gameEndType)
+                }
+
+                is KifAst.Move.Regular -> {
+                    val capturedKoma =
+                        posBeforeMove.getKoma(kifAstMove.endSq).getOrNull()
+                    Move.Regular(
+                        startSq = kifAstMove.startSq,
+                        endSq = kifAstMove.endSq,
+                        isPromotion = kifAstMove.isPromotion,
+                        side = side,
+                        komaType = kifAstMove.komaType,
+                        capturedKoma = capturedKoma
+                    )
+                }
+            }
+        }
+
+        private fun getSideToMove(
+            startingMoveNum: Int,
+            currentMoveNum: Int,
+            startingSide: Side,
+        ): Side {
+            // Assumes that moves alternate between sente and gote with no deviation
+            // (no passing moves, no game end, etc.)
+            return if ((startingMoveNum - currentMoveNum) % 2 == 0) {
+                startingSide
+            } else {
+                startingSide.switch()
+            }
+        }
+
+        private fun playOut(
+            variation: Variation<KifAst.Move>,
+            startingSide: Side,
+            startPos: Position,
+        ): Variation<Move> {
+            val movePositionPairs = variation.moves
+                .fold(emptyList<Pair<Move, Position>>()) { acc, kifAstMove ->
+                    val pos = acc.lastOrNull()?.second ?: startPos
+                    val move =
+                        moveFromKifAst(
+                            kifAstMove,
+                            variation.moveNum,
+                            startingSide,
+                            pos
+                        )
+                    acc + (move to pos.doMove(move))
+                }
+            val childVariations =
+                variation.branches.mapValues { (idx, children) ->
+                    children.map {
+                        val sideToMove = getSideToMove(
+                            variation.moveNum,
+                            variation.moveNum + idx,
+                            startingSide
+                        )
+                        playOut(it, sideToMove, movePositionPairs[idx].second)
+                    }
+                }
+            return Variation(
+                variation.moveNum,
+                movePositionPairs.map { it.first },
+                childVariations
+            )
+        }
+
+        private fun traverse(
+            acc: Variation<KifAst.Move>,
+            node: KifAst.Tree.MoveNode,
+        ): Variation<KifAst.Move> {
+            if (node.children.isEmpty()) {
+                return acc.add(node.move)
+            }
+            val mainVariation = traverse(
+                acc.add(node.move), node.children[0]
+            )
+            return node.children.subList(1, node.children.size)
+                .fold(mainVariation) { a, n ->
+                    a.addBranch(
+                        traverse(
+                            Variation(
+                                1 + node.move.moveNum,
+                                emptyList(),
+                                emptyMap()
+                            ), n
+                        )
+                    )
+                }
+        }
     }
+}
+
+private fun main() {
+    val game =
+        GameImpl.fromKifAst(readKifFile("sample_problems/variations.kif")!!)
+    println(game)
 }
